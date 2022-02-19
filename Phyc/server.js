63e2ce1029503c45cde406fcb4cb72823725b458
Tomkoid <67477750+Tomkoid@users.noreply.github.com>
2022-02-19 16:03:48 +0100

    Updated server.js to v3
    Lot of changes.

--- Phyc/server.js
+++ Phyc/server.js
@@ -7,8 +7,24 @@ const tmpconfig = fs.readFileSync("config.json");
 const config = JSON.parse(tmpconfig);
 
 server.all("/", (req, res) => {
-    console.log("\033[0;32m[HTTP server] Logged from " + req.ip.split(":")[3] + "\033[0m");
-    res.send(config.REPLIT_HTTPSERVERCODE);
+    let replithttpcode = config.REPLIT_HTTPSERVERCODE.toString();
+    if(config.REPLIT_HTTPSERVERCODE.startsWith("path:")) {
+        let websitepath = replithttpcode.split(":")[1];
+        if(!fs.existsSync(websitepath)) {
+            res.send("No " + websitepath + " file found");
+            return;
+        }
+        let webcode = fs.readFileSync(websitepath).toString();
+        if (config.REPLIT_HTTPSERVER_SHOW_LOGINS) {
+            console.log("\033[0;32m[HTTP server] Logged from " + req.ip.split(":")[3] + "\033[0m");
+        }
+        res.send(webcode);
+    } else {
+        if (config.REPLIT_HTTPSERVER_SHOW_LOGINS) {
+            console.log("\033[0;32m[HTTP server] Logged from " + req.ip.split(":")[3] + "\033[0m");
+        }
+        res.send(config.REPLIT_HTTPSERVERCODE);
+    }
 })
 
 function keepAlive() {
